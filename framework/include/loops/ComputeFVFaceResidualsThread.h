ec3bd11dcb295900614071460304a82e75ee5ac0
Robert Carlsen <rwcarlsen@gmail.com>
2019-12-20 11:12:18 -0700

    start sketching out finite volume loop

---
+++ framework/include/loops/ComputeFVFaceResidualsThread.h
@@ -0,0 +1,276 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ParallelUniqueId.h"
+#include "MooseMesh.h"
+#include "MooseTypes.h"
+#include "MooseException.h"
+#include "libmesh/libmesh_exceptions.h"
+#include "libmesh/elem.h"
+
+/**
+ * Base class for assembly-like calculations.
+ */
+template <typename RangeType>
+class ComputeFVFaceResidualsThread
+{
+public:
+  ComputeFVFaceResidualsThread(MooseMesh & mesh);
+
+  ComputeFVFaceResidualsThread(ComputeFVFaceResidualsThread & x, Threads::split split);
+
+  virtual ~ComputeFVFaceResidualsThread();
+
+  virtual void operator()(const RangeType & range, bool bypass_threading = false);
+
+  /**
+   * Called before the element range loop
+   */
+  virtual void pre();
+
+  /**
+   * Called after the element range loop
+   */
+  virtual void post();
+
+  /**
+   * Called before the boundary assembly
+   *
+   * @param elem - The element we are checking is on the boundary.
+   * @param side - The side of the element in question.
+   * @param bnd_id - ID of the boundary we are at
+   */
+  virtual void onSide(const Elem * elem, unsigned int side, const Elem * neighbor, Real area);
+
+  /**
+   * Called before the boundary assembly
+   *
+   * @param elem - The element we are checking is on the boundary.
+   * @param side - The side of the element in question.
+   * @param bnd_id - ID of the boundary we are at
+   */
+  virtual void onBoundary(const Elem * elem, unsigned int side, BoundaryID boundary, Real area);
+
+  /**
+   * Called every time the current subdomain changes (i.e. the subdomain of _this_ element
+   * is not the same as the subdomain of the last element).  Beware of over-using this!
+   * You might think that you can do some expensive stuff in here and get away with it...
+   * but there are applications that have TONS of subdomains....
+   */
+  virtual void subdomainChanged();
+
+  /**
+   * Called every time the neighbor subdomain changes (i.e. the subdomain of _this_ neighbor
+   * is not the same as the subdomain of the last neighbor).  Beware of over-using this!
+   * You might think that you can do some expensive stuff in here and get away with it...
+   * but there are applications that have TONS of subdomains....
+   */
+  virtual void neighborSubdomainChanged();
+
+  /**
+   * Called if a MooseException is caught anywhere during the computation.
+   * The single input parameter taken is a MooseException object.
+   */
+  virtual void caughtMooseException(MooseException &){};
+
+  /**
+   * Whether or not the loop should continue.
+   *
+   * @return true to keep going, false to stop.
+   */
+  virtual bool keepGoing() { return true; }
+
+protected:
+  MooseMesh & _mesh;
+  THREAD_ID _tid;
+
+  /// The subdomain for the current element
+  SubdomainID _subdomain;
+
+  /// The subdomain for the last element
+  SubdomainID _old_subdomain;
+
+  /// The subdomain for the current neighbor
+  SubdomainID _neighbor_subdomain;
+
+  /// The subdomain for the last neighbor
+  SubdomainID _old_neighbor_subdomain;
+};
+
+template <typename RangeType>
+ComputeFVFaceResidualsThread<RangeType>::ComputeFVFaceResidualsThread(MooseMesh & mesh)
+  : _mesh(mesh)
+{
+}
+
+template <typename RangeType>
+ComputeFVFaceResidualsThread<RangeType>::ComputeFVFaceResidualsThread(
+    ComputeFVFaceResidualsThread & x, Threads::split /*split*/)
+  : _mesh(x._mesh)
+{
+}
+
+template <typename RangeType>
+ComputeFVFaceResidualsThread<RangeType>::~ComputeFVFaceResidualsThread()
+{
+}
+
+// the vector<faceinfo> data structure needs to be built such that for all
+// sides on an interface between two subdomains, the elements of the same
+// subdomain are used consistently for all the "elem" (i.e. not "neighbor")
+// parameters in order to avoid jumping back and forth along the boundary
+// between using one or the other subdomains' FV kernels unpredictably.
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::operator()(const RangeType & range, bool bypass_threading)
+{
+  try
+  {
+    try
+    {
+      ParallelUniqueId puid;
+      _tid = bypass_threading ? 0 : puid.id;
+
+      pre();
+
+      _subdomain = Moose::INVALID_BLOCK_ID;
+      _neighbor_subdomain = Moose::INVALID_BLOCK_ID;
+
+      typename RangeType::const_iterator faceinfo = range.begin();
+      for (faceinfo = range.begin(); faceinfo != range.end(); ++faceinfo)
+      {
+        if (!keepGoing())
+          break;
+
+        // The faceinfo structure will probably also need the distance between
+        // cell centers of elements on each side of the side - to be used for
+        // computing gradients of variables at the interface.
+        const Elem * elem = faceinfo->elem;
+        unsigned int side = faceinfo->elem_side;
+        const Elem * neighbor = faceinfo->neighbor_elem;
+        unsigned int neighbor_side = faceinfo->neighbor_side;
+        Real area = faceinfo->common_face_area;
+
+        _old_subdomain = _subdomain;
+        _subdomain = elem->subdomain_id();
+        if (_subdomain != _old_subdomain)
+          subdomainChanged();
+
+        // get elem's face residual contribution to it's neighbor
+        onSide(elem, side, neighbor, area);
+
+        // boundary faces only border one element and so only contribute to
+        // one element's residual
+        std::vector<BoundaryID> boundary_ids = _mesh.getBoundaryIDs(elem, side);
+        for (auto it = boundary_ids.begin(); it != boundary_ids.end(); ++it)
+          onBoundary(elem, side, *it, area);
+      } // range
+
+      post();
+    }
+    catch (libMesh::LogicError & e)
+    {
+      throw MooseException("We caught a libMesh error");
+    }
+  }
+  catch (MooseException & e)
+  {
+    caughtMooseException(e);
+  }
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::pre()
+{
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::post()
+{
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::onBoundary(const Elem * elem,
+                                                    unsigned int side,
+                                                    BoundaryID bnd_id,
+                                                    Real area)
+{
+  std::vector<FVBoundaryCondition *> bcs;
+  _fe_problem.theWarehouse()
+      .query()
+      .condition<AttribSystem>("FVBC")
+      .condition<AttribBoundary>(bnd_id)
+      .queryInto(bcs);
+  if (bcs.size() == 0)
+    return;
+
+  _fe_problem.reinitElemFace(elem, side, bnd_id, _tid);
+
+  // Set up Sentinel class so that, even if reinitMaterialsFace() throws, we
+  // still remember to swap back during stack unwinding.
+  SwapBackSentinel sentinel(_fe_problem, &FEProblem::swapBackMaterialsFace, _tid);
+
+  _fe_problem.reinitMaterialsFace(elem->subdomain_id(), _tid);
+  _fe_problem.reinitMaterialsBoundary(bnd_id, _tid);
+
+  for (const auto & bc : bcs)
+    bc->computeResidual();
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::onSide(const Elem * elem,
+                                                unsigned int side,
+                                                const Elem * neighbor,
+                                                Real area)
+{
+  std::vector<FVKernel *> kernels;
+  _fe_problem.theWarehouse()
+      .query()
+      .condition<AttribSystem>("FVKernel")
+      .condition<AttribSubdomains>(_subdomain)
+      .queryInto(kernels);
+  if (kernels.size() == 0)
+    return;
+
+  _fe_problem.reinitNeighbor(elem, side, _tid);
+
+  // Set up Sentinels so that, even if one of the reinitMaterialsXXX() calls throws, we
+  // still remember to swap back during stack unwinding.
+  SwapBackSentinel face_sentinel(_fe_problem, &FEProblem::swapBackMaterialsFace, _tid);
+  _fe_problem.reinitMaterialsFace(elem->subdomain_id(), _tid);
+
+  SwapBackSentinel neighbor_sentinel(_fe_problem, &FEProblem::swapBackMaterialsNeighbor, _tid);
+  _fe_problem.reinitMaterialsNeighbor(neighbor->subdomain_id(), _tid);
+
+  for (const auto k : kernels)
+    k->computeResidual();
+
+  {
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    _fe_problem.addResidualNeighbor(_tid);
+  }
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::subdomainChanged()
+{
+}
+
+template <typename RangeType>
+void
+ComputeFVFaceResidualsThread<RangeType>::neighborSubdomainChanged()
+{
+}
