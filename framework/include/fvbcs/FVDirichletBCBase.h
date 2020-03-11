08b6c0981fa4610bc43ded5ffc09b722263a8169
schuseba <sebastian.schunert@inl.gov>
2020-03-11 17:57:15 -0600

    FVBCs (not complete to any degree, missing FVIntegratedBC)

---
+++ framework/include/fvbcs/FVDirichletBCBase.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVDirichletBCBase.h"
+
+/**
+ * Base class for
+ */
+class FVDirichletBCBase : public FVBoundaryCondition
+{
+public:
+  /**
+   * Class constructor.
+   * @param parameters The InputParameters for the object
+   * @param nodal Whether this BC is applied to nodes or not
+   */
+  FVDirichletBCBase(const InputParameters & parameters);
+
+  static InputParameters validParams();
+
+protected:
+  virtual Real boundaryValue() = 0;
+};
