70dbb1aa4f1fef521df7232214513f83ff457ea1
Robert Carlsen <rwcarlsen@gmail.com>
2020-03-18 14:56:24 -0600

    Fixed some segfaults and bad indexing bugs.
    There are still problems with material properties - some strange AD
    property syncing problems as well as the copying props to the ghosted
    neighbor material data object not working.

--- framework/include/loops/ComputeFVFluxThread.h
+++ framework/include/loops/ComputeFVFluxThread.h
@@ -262,6 +262,11 @@ ComputeFVFluxThread<RangeType>::reinitVariables(const FaceInfo & fi)
   // most of that is (obviously) FE specific.  Also - we need to fall back to
   // reiniting everything like normal if there is any FV-FE variable coupling.
   _fe_problem.prepare(&fi.leftElem(), _tid);
+  //_fe_problem.reinitElem(&fi.leftElem(), _tid);
+
+  // I'm not sure of any other way to get the qRuleFace to be initialized
+  // other than this direct call.  How does it work for DG?
+  _fe_problem.assembly(_tid).reinit(&fi.leftElem(), fi.leftSideID());
 
   // TODO: this triggers a bunch of FE-specific stuff to occur that we might
   // not need if only FV variables are active.  Some of the stuff triggered by
@@ -286,7 +291,7 @@ ComputeFVFluxThread<RangeType>::reinitVariables(const FaceInfo & fi)
   for (auto var : _needed_fv_vars)
     var->computeFaceValues(fi);
 
-  // TODO: do we really need both reinitMaterials and reinitMaterialsFace - I
+  // TODO: do we really need both reinitMaterials and reinitMaterialsFace? - I
   // think we do need both.
   _fe_problem.reinitMaterials(fi.leftElem().subdomain_id(), _tid);
   _fe_problem.reinitMaterialsFace(fi.leftElem().subdomain_id(), _tid);
@@ -304,7 +309,10 @@ ComputeFVFluxThread<RangeType>::reinitVariables(const FaceInfo & fi)
     // neighbor (face) material properties data structure.
     auto dst = _fe_problem.getMaterialData(Moose::NEIGHBOR_MATERIAL_DATA, _tid);
     auto src = _fe_problem.getMaterialData(Moose::BOUNDARY_MATERIAL_DATA, _tid);
-    dst->copyPropsFrom(*src);
+
+    // FIXME/TODO: this needs to be enabled/uncommented, but it currently
+    // fails due to differing number of qp's for props between src and dst.
+    //dst->copyPropsFrom(*src);
   }
 
   // this is the swap-back object - don't forget to catch it into local var
@@ -335,16 +343,6 @@ ComputeFVFluxThread<RangeType>::onFace(const FaceInfo & fi)
     return;
 
   auto swap_back_sentinel = reinitVariables(fi);
-
-  // Set up Sentinels so that, even if one of the reinitMaterialsXXX() calls throws, we
-  // still remember to swap back during stack unwinding.
-  SwapBackSentinel face_sentinel(_fe_problem, &FEProblem::swapBackMaterialsFace, _tid);
-  _fe_problem.reinitMaterialsFace(fi.leftElem().subdomain_id(), _tid);
-
-  // this reinits the materials for the neighbor element on the face (although it may not look like
-  // it)
-  SwapBackSentinel neighbor_sentinel(_fe_problem, &FEProblem::swapBackMaterialsNeighbor, _tid);
-  _fe_problem.reinitMaterialsNeighbor(fi.rightElem().subdomain_id(), _tid);
 
   for (const auto k : kernels)
     if (_do_jacobian)
